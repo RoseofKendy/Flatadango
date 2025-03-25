410e67f92cea07a7ac2be05f1e949ba915254051
Sharon Kendi <sharonkendi35@gmail.com>
2025-03-25 12:01:30 +0300

    Fixes server load

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Base URL for JSON Server
-const BASE_URL = 'https://flatadango-api.onrender.com/films';
+const BASE_URL = 'https://flatadango-api.onrender.com/';
 
 // DOM elements
 const filmList = document.getElementById('films');
